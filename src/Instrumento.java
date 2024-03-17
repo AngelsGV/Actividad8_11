d84ad3eb61f6df4f82bf9242b2d2ca4471189f48
AngelsGV <angels.not99@gmail.com>
2024-03-17 17:51:34 +0100

    Initial commit

---
+++ src/Instrumento.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public abstract class Instrumento {
+protected Nota[] melodia;
+public Instrumento(){
+    melodia = new Nota[0];
+}
+void add(Nota n){
+    melodia= Arrays.copyOf(melodia,melodia.length+1);
+    melodia[melodia.length-1] = n;
+}
+abstract void interpretar();
+}
---
+++ src/Nota.java
@@ -0,0 +1,1 @@
+public enum Nota {DO, RE, MI, FA, SOL, LA, SI}
